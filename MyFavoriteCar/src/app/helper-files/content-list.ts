4d24611388f1d8058cdbbbb27ac1a727437ea81f
Zhijun Zhou <zz@myscc.ca>
2022-04-30 17:53:46 -0400

    created functions (add, number)

--- MyFavoriteCar/src/app/helper-files/content-list.ts
+++ MyFavoriteCar/src/app/helper-files/content-list.ts
@@ -6,7 +6,19 @@ public constructor() {
   //initialize the _cars
   this._cars = [];
 }
+
 // define the getter
 get cars(): Content[] {
   return this._cars = [];
+  }
+
+  // add new car to car list
+  public addCar(car: Content) {
+    this._cars?.push(car);
+  }
+
+  //return the number of list of cars
+  public numberOfCars(): Number{
+    return (this._cars?).length;
+  }
 }
