d731f64d4477738dfec1dc1ce426ba837ecc43ee
Zhijun Zhou <zz@myscc.ca>
2022-04-30 22:44:15 -0400

    update content-list

--- MyFavoriteCar/src/app/app.module.ts
+++ MyFavoriteCar/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
+import { ContentCardComponent } from './content-card/content-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentComponent,
+    ContentCardComponent,
   ],
   imports: [
     BrowserModule
