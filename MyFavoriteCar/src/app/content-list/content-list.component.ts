c587a0c9fe9e7d4ca94071ba81a51cfab538e8b3
Zhijun Zhou <zz@myscc.ca>
2022-05-03 12:17:25 -0400

    Update content-list.component.ts

--- MyFavoriteCar/src/app/content-list/content-list.component.ts
+++ MyFavoriteCar/src/app/content-list/content-list.component.ts
@@ -66,7 +66,7 @@ getHtml(index: number): string{
       type: 'suv',
       tags:['black', '5 seats']
     });
-    this.addCar(      {
+    this.addCar({
       id:3,
       title: 'Escape',
       description: 'Ford produces cars',
@@ -74,6 +74,41 @@ getHtml(index: number): string{
       imgURL: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/2020_Ford_Escape_SEL%2C_front_7.11.20.jpg',
       type: 'suv',
       tags:['awd', '5 seats']
+    });
+    this.addCar({
+      id:4,
+      title: 'Q5',
+      description: 'Audi produces cars',
+      creator: 'Audi',
+      imgURL: 'https://cars.usnews.com/static/images/Auto/izmo/i159614344/2021_audi_q5_angularfront.jpg',
+      tags:['awd', '5 seats']
+    });
+    this.addCar({
+      id:5,
+      title: '300',
+      description: 'Chrysler produces cars',
+      creator: 'Chrysler',
+      imgURL: 'https://www.motortrend.com/uploads/sites/5/2021/05/2021-chrysler-300-exterior-01.jpg',
+      type: 'sedan',
+      tags:['forward drive', '7 seats']
+    });
+    this.addCar({
+      id:6,
+      title: 'Century',
+      description: 'Buick produces cars',
+      creator: 'Buick',
+      imgURL: 'https://media.ed.edmunds-media.com/buick/century/1997/oem/1997_buick_century_sedan_custom_fq_oem_1_500.jpg',
+      type: 'sedan',
+      tags:['forward', '5 seats']
+    });
+    this.addCar({
+      id:7,
+      title: 'F150',
+      description: 'Ford produces cars',
+      creator: 'Ford',
+      imgURL: 'https://media.ford.com/content/fordmedia/fna/us/en/news/2020/06/25/built-for-getting-things-done-ford-f150/jcr:content/image.img.881.495.jpg/1600201093702.jpg',
+      type: 'truck',
+      tags:['awd', '6 seats']
     });
   }
 
