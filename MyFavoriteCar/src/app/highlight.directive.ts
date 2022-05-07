976949b2774260b3882dcae665c8eb5a870c4b40
Zhijun Zhou <zz@myscc.ca>
2022-05-07 19:12:57 -0400

    update highlight directive

--- MyFavoriteCar/src/app/highlight.directive.ts
+++ MyFavoriteCar/src/app/highlight.directive.ts
@@ -16,15 +16,20 @@ export class HighlightDirective {
   else if(tag.toLowerCase() == "span")
   this.el.nativeElement.style.fontWeight = "bold"
  */
+  let className = this.el.nativeElement.class;
+  if(className =="type")
   this.el.nativeElement.style.textDecoration = "underline";
+  if (className == "tags")
+  this.el.nativeElement.style.fontWeight = "bold";
   }
 
 
   @HostListener('mouseleave') onMouseLeave() {
-    // if (this.el.nativeElement.tagName.toLowerCase() == 'div')
-    this.el.nativeElement.style.textDecoration =  '';
-    // else if(this.el.nativeElement.tagName.toLowerCase() == 'span')
-    // this.el.nativeElement.style.fontWeight = 'normal';
+    let className = this.el.nativeElement.class;
+    if(className =="type")
+    this.el.nativeElement.style.textDecoration = "";
+    if (className == "tags")
+    this.el.nativeElement.style.fontWeight = "normal";
   }
 
   private highlight(color: string) {
