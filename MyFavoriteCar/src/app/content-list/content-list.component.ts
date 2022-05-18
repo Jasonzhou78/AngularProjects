014a08aa70512e72530aff1dde36e94dd388503a
Zhijun Zhou <zz@myscc.ca>
2022-05-18 13:21:39 -0400

    create a content-db file for cars container

--- MyFavoriteCar/src/app/content-list/content-list.component.ts
+++ MyFavoriteCar/src/app/content-list/content-list.component.ts
@@ -58,67 +58,6 @@ getHtml(index: number): string{
 
 
 
-  ngOnInit(): void {
-
-    this.addCar({ id:1,
-      title: 'Accord',
-      description: 'Honda produces cars',
-      creator: 'Honda',
-      type: 'Sedan',
-      /* tags:['blue', '5 seats'] */
-    });
-    this.addCar({
-      id:2,
-      title: 'RAV4',
-      description: 'Toyota produces cars',
-      creator: 'Toyota',
-      imgURL: 'https://images.hgmsites.net/med/2022-toyota-rav4-hybrid-se-awd-natl-angular-front-exterior-view_100838633_m.jpg',
-      type: 'SUV',
-      tags:['Back', '5 Seats']
-    });
-    this.addCar({
-      id:3,
-      title: 'Escape',
-      description: 'Ford produces cars',
-      creator: 'Ford',
-      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/2020_Ford_Escape_SEL%2C_front_7.11.20.jpg',
-      type: 'SUV',
-      tags:['AWD', '5 Seats']
-    });
-    this.addCar({
-      id:4,
-      title: 'Q5',
-      description: 'Audi produces cars',
-      creator: 'Audi',
-      imgURL: 'https://cars.usnews.com/static/images/Auto/izmo/i159614344/2021_audi_q5_angularfront.jpg',
-      tags:['AWD', '5 Seats']
-    });
-    this.addCar({
-      id:5,
-      title: '300',
-      description: 'Chrysler produces cars',
-      creator: 'Chrysler',
-      imgURL: 'https://www.motortrend.com/uploads/sites/5/2021/05/2021-chrysler-300-exterior-01.jpg',
-      type: 'Sedan',
-      tags:['Forward', '7 Seats']
-    });
-    this.addCar({
-      id:6,
-      title: 'Envision',
-      description: 'Buick produces cars',
-      creator: 'Buick',
-      imgURL: 'https://s3-prod-canada.autonews.com/s3fs-public/2020BuickEncoreGX-MAIN_i.jpg',
-      type: 'Sedan',
-      tags:['Forward', '5 Seats']
-    });
-    this.addCar({
-      id:7,
-      title: 'F150',
-      description: 'Ford produces cars',
-      creator: 'Ford',
-      type: 'Truck',
-      tags:['AWD', '6 Seats']
-    });
-  }
+  ngOnInit(){}
 
 }
