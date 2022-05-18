c992ca15ea5a8ba2c370e76ac52b0d15b3aa4cf2
Zhijun Zhou <zz@myscc.ca>
2022-05-18 13:40:10 -0400

    annotate the service class

--- MyFavoriteCar/src/app/car.service.ts
+++ MyFavoriteCar/src/app/car.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 
+
+// create a injectable class called CarService
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'   //the root indicate the class can be used by any class in the application
 })
 export class CarService {
 
